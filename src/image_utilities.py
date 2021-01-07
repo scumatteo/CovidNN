83a672feb0f7c06033199a318a3fb1d97bc814f2
scumatteo <matteo.scucchia@studio.unibo.it>
2021-01-07 17:27:21 +0100

    Corrected function for images visualization

--- src/image_utilities.py
+++ src/image_utilities.py
@@ -17,9 +17,18 @@ def get_img_array(img_path, size):
 
 
 def vis_images(images, titles, n_columns):
-    fig, ax = plt.subplots(len(images)//n_columns, n_columns, figsize=(10,10))
-    for i in len(images):
+    n_rows = len(images)//n_columns
+    n_cols = n_columns
+    fig, ax = plt.subplots(n_rows, n_cols, figsize=(15,15))
+    if n_rows == 1:
+      for i in range(n_cols):
         ax[i].imshow(images[i])
         ax[i].set_title(titles[i])
-        plt.tight_layout()
-        plt.show()
+    else:
+      for i in range(n_rows):
+        for j in range(n_cols):
+          pos = j + i*n_cols
+          ax[i, j].imshow(images[pos])
+          ax[i, j].set_title(titles[pos])
+          plt.tight_layout()
+          plt.show()
